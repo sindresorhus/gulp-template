5da03c90ecaf9584af424e3f3683a304c11ecfe8
Kahlil Lechelt <hello@kahlil.co>
2014-01-07 08:48:47 +0100

    Export a named function.

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ var gutil = require('gulp-util');
 var tpl = require('lodash.template');
 var argv = require('optimist').argv;
 
-module.exports = function (data, options) {
+// The plugin name as a constant.
+var PLUGIN_NAME = 'gulp-template';
+
+function GulpTemplate(data, options, fromCli) {
 	return es.map(function (file, cb) {
 		file.contents = new Buffer(tpl(file.contents.toString(), data, options));
 		cb(null, file);
 	});
 };
+module.exports = GulpTemplate;
