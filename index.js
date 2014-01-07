314f9b9540039f0c1cbcdd13afe77a97cdab2b9b
Kahlil Lechelt <hello@kahlil.co>
2014-01-07 08:47:30 +0100

    Require optimist for cli options parsing.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var es = require('event-stream');
 var gutil = require('gulp-util');
 var tpl = require('lodash.template');
+var argv = require('optimist').argv;
 
 module.exports = function (data, options) {
 	return es.map(function (file, cb) {
