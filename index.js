25097e1c74470bf5d830f6716c223cb90765fed6
Kahlil Lechelt <hello@kahlil.co>
2014-01-07 08:49:34 +0100

    Add functionality to use cli options as template data.

--- index.js
+++ index.js
@@ -8,9 +8,25 @@ var argv = require('optimist').argv;
 var PLUGIN_NAME = 'gulp-template';
 
 function GulpTemplate(data, options, fromCli) {
+
+	if (typeof data === 'boolean') {
+		fromCli = data;
+	} else if (typeof options === 'boolean') {
+		fromCli = options;
+	}
+
+	if (fromCli) {
+		if (argv) {
+			data = argv;
+		} else {
+			throw new gutil.PluginError(PLUGIN_NAME, 'Please provide cli options.');
+		}
+	}
+
 	return es.map(function (file, cb) {
 		file.contents = new Buffer(tpl(file.contents.toString(), data, options));
 		cb(null, file);
 	});
 };
+
 module.exports = GulpTemplate;
